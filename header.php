d429086821ba481699256700bc20ca099559db34
andrew-merrell <andrew.merrell@malvernpanalytical.com>
2026-04-15 11:25:39 +0100

    Update login link to main website Login page

--- header.php
+++ header.php
@@ -49,7 +49,7 @@
 
     <div class="c-header__group">
       <?php if (!$is_logged_in) : ?>
-        <a class="c-button c-button--blue c-button--small" href="<?php echo esc_url(wp_login_url()); ?>">Login</a>
+        <a class="c-button c-button--blue c-button--small" href="<?php echo esc_url('https://www.malvernpanalytical.com/en/support/login?referrer=' . urlencode('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'])); ?>">Login</a>
         <a class="c-button c-button--outline-white c-button--small" href="<?php echo esc_url(wp_registration_url()); ?>">Register</a>
       <?php else : ?>
         <a class="c-button c-button--outline-white c-button--small" href="<?php echo esc_url(wp_logout_url(home_url('/'))); ?>">Log out</a>
@@ -151,7 +151,7 @@
 
     <div class="c-header__group">
       <?php if (!$is_logged_in) : ?>
-        <a class="c-button c-button--blue c-button--small" href="<?php echo esc_url(wp_login_url()); ?>">Login</a>
+        <a class="c-button c-button--blue c-button--small" href="<?php echo esc_url('https://www.malvernpanalytical.com/en/support/login?referrer=' . urlencode('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'])); ?>">Login</a>
         <a class="c-button c-button--outline-white c-button--small" href="<?php echo esc_url(wp_registration_url()); ?>">Register</a>
       <?php else : ?>
         <a class="c-button c-button--outline-white c-button--small" href="<?php echo esc_url(wp_logout_url(home_url('/'))); ?>">Log out</a>
